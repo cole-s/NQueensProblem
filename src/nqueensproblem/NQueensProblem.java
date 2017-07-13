8b6583c2383d5da542e7e148fa892d8827eba200
COLES-SURFACE\casch <scha7843@kettering.edu>
2017-07-13 14:24:58 -0400

    Added NQueensProblem.java file to the repository.
    To Do:
       * Need to add code for the basic User interface
       * Add Queen and Control Class

---
+++ src/nqueensproblem/NQueensProblem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nqueensproblem;
+
+/**
+ * Main Class for use of User Interface calls to Control
+ * @author scha7843
+ */
+public class NQueensProblem {
+
+    /**
+     * @param args the command line arguments
+     * @author scha7843
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
