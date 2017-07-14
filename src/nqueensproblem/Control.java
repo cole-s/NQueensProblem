07e148a26d259dc41143bb45d5c21140080a403a
COLES-SURFACE\casch <scha7843@kettering.edu>
2017-07-13 20:53:43 -0400

    Added Control and Queen Classes
    To Do:
       -Create data elements of Queen class
       -Create array of Queens in Control class

---
+++ src/nqueensproblem/Control.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nqueensproblem;
+
+/**
+ * Control Class handles all user interface interactions. Makes calls to Queens
+ * Class. Handles all interaction checking and to see if the problem has been
+ * solved.
+ * 
+ * @author scha7843
+ */
+public class Control {
+    
+}
---
+++ src/nqueensproblem/Queen.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nqueensproblem;
+
+/**
+ * Queen Class handles all of the data needed for tracking position and all
+ * intersections with other Queens
+ * @author scha7843
+ */
+public class Queen {
+    
+} // end of Queen Class
