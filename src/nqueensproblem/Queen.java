84e26b60feb263adc37603dbf4dd3625a2aab295
COLES-LAPTOP\casch <scha7843@kettering.edu>
2017-07-15 17:50:01 -0400

    Updated the Queen class to include all private data members to allow easier storage of n number of Queens for problem.

--- src/nqueensproblem/Queen.java
+++ src/nqueensproblem/Queen.java
@@ -6,5 +6,20 @@ package nqueensproblem;
  * @author scha7843
  */
 public class Queen {
+    private int row; // stores row value of Queen
+    private int collumn; // stores collumn value of Queen
+    private boolean validlocal; //is a valid location boolean for Queen object
     
+    /**
+     * Getters and Setters for the private variables in the Queen class
+     * 
+     * Parameters for setters are integers and booleans
+     * returns integers and booleans for getters
+     */
+    public int getRow(){ return row; }
+    public void setRow(int num){ row = num; }
+    public int getCollumn() { return collumn; }
+    public void setCollumn(int num){ collumn = num; }
+    public boolean getValidLocal(){ return validlocal; }
+    public void setValidLocal(boolean bool) { validlocal = bool; }
 } // end of Queen Class
